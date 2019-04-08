0c46dfcf91ba3b590660a847e1f279c68c0ba7e4
mdalqamaiqbal <alqama.ceb@123>
2019-04-08 01:54:32 +0100

    comments

--- js/post.js
+++ js/post.js
@@ -26,17 +26,42 @@ function createModal(myModel, myBtn){
 }
 
 function setEditableTrue(editbtn){
+  var title = document.getElementById("blogTitleNew");
   var content = document.getElementById("blogBody");
   var savebtn = document.getElementById("savebtn");
   content.contentEditable = true;
+  title.contentEditable = true;
   editbtn.style.display = 'none';
   savebtn.style.display = 'block';
 }
 
-function setEditableFalse(savebtn){
+function setEditableFalse(savebtn) {
+  var title = document.getElementById("blogTitleNew");
   var content = document.getElementById("blogBody");
   var editbtn = document.getElementById("editbtn");
+  title.contentEditable = false;
   content.contentEditable = false;
   savebtn.style.display = 'none';
   editbtn.style.display = 'block';
+}
+
+function like() {
+  var like = document.getElementById("like");
+  var liked = document.getElementById("liked");
+  console.log(like);
+  like.style.display = 'none';
+  liked.style.display = 'block';
+}
+
+function comment() {
+  var comment = document.getElementById("commentText").value;
+  if(comment !== "") {
+    var classComment = document.getElementById("comment");
+    var text = document.createTextNode(comment);
+    var div = document.createElement("div");
+    div.setAttribute('class', 'comment');
+    div.style.display = 'inline-block';
+    div.appendChild(text);
+    classComment.appendChild(div);
+  }
 }
